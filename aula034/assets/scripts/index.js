5f9e4b21e52ad42fdb6cdf556e6987da4fda1d71
= <=>
2024-12-30 15:46:33 -0300

    aula sobre DOM

---
+++ aula034/assets/scripts/index.js
@@ -0,0 +1,35 @@
+// Capturando container
+const container = document.querySelector('.container')
+
+// Criando elemento novo
+const divNova = document.createElement('div')
+divNova.className = 'divNova'
+
+// Array de objetos
+const elementosHtml = [
+    {tag: 'p', texto: '<p>Isso é um paragráfo</p>'},
+    {tag: 'div', texto: '<div>Isso é uma div</div>'},
+    {tag: 'main', texto: '<main>Isso é o conteúdo principal</main>'},
+    {tag: 'section', texto: '<section>Isso é uma seção</section>'},
+    {tag: 'footer', texto: '<footer>Isso é um rodapé</footer>'}
+]
+
+// Tamanho do array de objetos
+const numeroDeElementos = elementosHtml.length
+
+// O for que começa em 0 e vai incrementando até estar igual ao tamanho da array
+for(i = 0; i < numeroDeElementos; i++){
+    const {tag, texto} = elementosHtml[i] // capturando a tag e o texto via desestruturação
+    const elementoCriado = document.createElement(tag) // recebe o elemento do objeto para criado
+
+    elementoCriado.textContent = texto // adicionando o texto da propriedade texto ao elemento criado
+    elementoCriado.className = 'vermelhoOneBit' // classe para estilização o elemento
+    
+    divNova.appendChild(elementoCriado) // Adicionando elemento ao container
+}
+
+container.appendChild(divNova) // adicionando div ao container
+
+// DOM - Document Object Model
+
+// O objeto que é o pai de todos os outros elementos é o window, o primeiro de todos, a ordem ficaria basicamente window.document.body e assim ia seguindo. Chamandos os elementos 'pai' de parent e os elementos filhos de 'child'
